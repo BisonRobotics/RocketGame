ebb2c6f0613a55c2b5ff1336547ed64c109eead1
RubenTipparach <santipab.stephane@gmail.com>
2017-09-25 23:01:49 -0500

    initial comit of project files

---
+++ Assets/RocketMan/Scripts/FollowRocket.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowRocket : MonoBehaviour {
+
+    [SerializeField]
+    Transform target;
+
+    float initY;
+
+	// Use this for initialization
+	void Start () {
+        initY = transform.position.y;
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        transform.position = new Vector3(target.position.x, target.position.y + initY, transform.position.z);
+	}
+}
---
+++ Assets/RocketMan/Scripts/RocketController.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using ps = UnityEngine.ParticleSystem;
+
+public class RocketController : MonoBehaviour {
+
+    float startEmission = 0;
+
+    [SerializeField]
+    ParticleSystem particles;
+
+    ps.EmissionModule emmod;
+
+    Rigidbody rb;
+
+    [SerializeField]
+    float pitchSensitivity;
+
+    [SerializeField]
+    float thrustUp;
+
+    // Use this for initialization
+    void Start () {
+        startEmission = particles.emission.rateOverTime.constant;
+        emmod = particles.emission;
+
+        rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+
+    }
+
+    // Update called in the physics update (50 times persecond)
+    void FixedUpdate () {
+		
+        if (Input.GetKey(KeyCode.W))
+        {
+            emmod.rateOverTimeMultiplier = startEmission;
+            rb.AddRelativeForce(0, thrustUp, 0);
+        }
+        else
+        {
+            emmod.rateOverTimeMultiplier = 0;
+        }
+
+        if(Input.GetKey(KeyCode.A))
+        {
+            rb.AddRelativeTorque(0, 0, pitchSensitivity);
+        }
+
+        if (Input.GetKey(KeyCode.D))
+        {
+            rb.AddRelativeTorque(0, 0, -pitchSensitivity);
+        }
+    }
+}
